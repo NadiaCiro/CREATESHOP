7a4e4b50991eb7d5700418f8e7b47c0ce6efd459
NadiaCiro <luciguci262006@gmail.com>
2024-01-03 12:24:25 -0500

    backend

---
+++ src/app.js
@@ -0,0 +1,36 @@
+/*Tiene todas las configuraciones de express y llama a las rutas */
+import express from 'express'
+import usersRoutes from './routes/users.routes.js';
+import indexRoutes from "./routes/index.routes.js"
+import productsRoutes from "./routes/products.routes.js";
+import searchRoutes from "./routes/search.routes.js";
+import cors from "cors";
+import featuredProductsRoutes from './routes/featured.routes.js';
+
+const app = express()
+
+
+var corsOptions = {
+    origin: 'http://127.0.0.1:4200',
+    optionsSuccessStatus: 200 // For legacy browser support
+}
+app.use(cors())/*Api consumible, comunicar al front */
+
+app.use(express.json(corsOptions))/*Primero se recibe los datos se convierten a json o un objeto js y luego se pasa a las rutas */
+
+
+/*Rutas */
+app.use(indexRoutes)
+app.use('/api',usersRoutes)
+app.use('/api', productsRoutes)
+app.use('/api', featuredProductsRoutes)
+app.use('/api', searchRoutes)
+
+/*Not found Route */
+app.use((req, res, next) => {
+    res.status(404).json({
+        message: 'Endpoint not found'
+    })
+}) 
+
+export default app;
