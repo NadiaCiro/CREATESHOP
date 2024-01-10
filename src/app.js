1d115a4385c809b251de80295723654f00947886
NadiaCiro <luciguci262006@gmail.com>
2024-01-10 16:21:41 -0500

    origin

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 var corsOptions = {
-    origin: 'http://127.0.0.1:4200',
+    origin: 'https://createshop.vercel.app',
     optionsSuccessStatus: 200 // For legacy browser support
 }
 
